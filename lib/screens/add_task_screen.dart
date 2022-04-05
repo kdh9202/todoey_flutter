19b68d003b30e0f8e16bcc50aa5c0528ed42d3e7
kdh9202 <kdh9202@gmail.com>
2022-04-06 07:08:31 +0900

    202204060708

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AddTaskScreen extends StatefulWidget {
@@ -10,6 +11,37 @@ class AddTaskScreen extends StatefulWidget {
 class _AddTaskScreenState extends State<AddTaskScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: const Color(0xff757575),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.lightBlueAccent),
+            ),
+            const TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Add'),
+              style: ElevatedButton.styleFrom(primary: Colors.lightBlueAccent),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
